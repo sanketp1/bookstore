ddf57b66afbfccbbf5c079bba667fedfac268325
Sanket Patil <psanket18052001@gmail.com>
2023-03-06 23:59:08 +0530

    added fonts and colors for app

---
+++ lib/res/appColor.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static final titleColor = Colors.black;
+  static final descriptionTextColor = Colors.black87;
+  static final splashScreenColor = Colors.deepPurple;
+}
